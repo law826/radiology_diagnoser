f6d95e493f080b2d72e673d35b3ae777e9521a4c
Lawrence Ngo <law826@gmail.com>
2013-07-04 11:19:17 -0400

    Initial commit

---
+++ radiology_diagnoser.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+radiology_diagnoser.py
+
+Started by LN on 7/4/13
+"""
+
+
+
+
+import os
+import sys
+import random as rand
+from Tkinter import *
+import tkMessageBox
+import tkFileDialog 
+import cPickle
+import numpy as np
+import igraph
+import getpass
+from pdb import *
+
+class DataBase:
+	def __init__(self, mainwindow):
+		self.mainwindow = mainwindow
+		self.load_user_settings()
+		try:
+			self.load_graph()
+		except (cPickle.UnpicklingError):
+			pass
+
+	def load_user_settings(self):
+		try: 
+			self.user_settings = cPickle.load(open('user_settings.p', 'rb'))
+			if os.getcwd() in self.user_settings:
+				self.save_path = self.user_settings[os.getcwd()]
+			else:
+				tkMessageBox.showinfo("New User/Computer Detected. Please choose a save directory.")	
+				self.SetPath()
+		except (IOError, cPickle.UnpicklingError):
+			self.user_settings = dict()
+			tkMessageBox.showinfo("New User/Computer Detected", "Please choose a save directory.")
+			self.SetPath()
+
+	def load_graph(self):
+		self.g = igraph.Graph.Read_Pickle(os.sep.join([self.save_path, "graph.p"]))
+
+				
+	def save_graph(self):
+		try:
+			self.g.write_pickle(os.sep.join([self.save_path, "graph.p"]))
+		except:
+			tkMessageBox.showerror("Tkinter Entry Widget", "Enter a valid save path (current path is %s)" %self.save_path)
+
+
+class MainWindow:
+	def __init__(self):
+
+		self.MakeUI()
+			
+	def MakeUI(self):
+		self.root = Tk()
+
+		self.root.lift()
+		self.DB = DataBase(self) # Instantiated here at the end because of parent window issues for ask directory widget.
+		self.root.mainloop()
