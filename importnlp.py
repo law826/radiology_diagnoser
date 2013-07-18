39a94212bb4fe182386c6223c89ff08f4dd4ec00
Lawrence Ngo <law826@gmail.com>
2013-07-17 20:07:08 -0400

    update

---
+++ importnlp.py
@@ -0,0 +1,10 @@
+from __future__ import division
+import os, sys, random as rand, tkMessageBox, tkFileDialog, cPickle, numpy as np, getpass, tkentrycomplete as tkcomp, re, nltk
+from Tkinter import *
+from pdb import *
+from igraph import *
+
+
+import basefunctions as bf
+import nltk
+
