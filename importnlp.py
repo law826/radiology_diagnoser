f21b911492f9930ebadab42679f995682fd6e73a
Lawrence Ngo <law826@gmail.com>
2013-07-20 12:51:10 -0400

    Extend Brackets and Curlys

--- importnlp.py
+++ importnlp.py
@@ -19,41 +19,51 @@ class ImportNLP:
 		self.string2 = string2
 		self.replaced = body.replace(self.string1, self.string2)
 
-	def FindWordsInBrackets(self, body):
+	def FindWordsInBracketsAndCurlies(self, body):
+		"""
+		Returns words without brackets in self.diagnoses and self.symptoms.
+		"""
 		self.diagnoses = [m.group(1) for m in re.finditer(r"\[([A-Za-z0-9_ \(\)\-]+)\]", body)]
 		self.symptoms = [m.group(1) for m in re.finditer(r"\{([A-Za-z0-9_ \(\)\-]+)\}", body)]
 
-	def ExtendBrackets(self, list_of_terms, target_body, file=None):
+	def ExtendBrackets(self, list_of_terms, target_body):
 		"""
+		Run FindWordsInBracketsAndCurlies first.
 		Adds brackets to the same words if they have not yet received brackets.
 		"""
-
 		target_body = ' ' + target_body + ' '
 
 		for i, term in enumerate(list_of_terms):
 			bracketed_term = ' [' + term + '] '
 			if i ==0:
-				self.eboutput = re.sub(r"[^[]%s[^]]" %term, bracketed_term, target_body); print self.eboutput
+				self.eboutput = re.sub(r"[^[]%s[^]]" %term, bracketed_term, target_body); #print self.eboutput
 			else:
-				self.eboutput = re.sub(r"[^[]%s[^]]" %term, bracketed_term, self.eboutput); print self.eboutput
+				self.eboutput = re.sub(r"[^[]%s[^]]" %term, bracketed_term, self.eboutput); #print self.eboutput
+
+	def ExtendCurlys(self, list_of_terms, target_body):
+		"""
+		Run FindWordsInBracketsAndCurlies first.
+		Adds brackets to the same words if they have not yet received brackets.
+		"""
+		target_body = ' ' + target_body + ' '
 
 
-
-		# Open a file for reading and another for writing.
-		
-
-		# _fibromas = re.compile('are')
-		# #match = _fibromas.finditer(self.raw)
-		# set_trace()
-		# [m.group(1) for m in re.finditer(_fibromas, self.raw)]
-		
-		# set_trace()
-		
-		
-		# Take two words and replace all instances of that the second word with the first word.
+		for i, term in enumerate(list_of_terms):
+			curly_term = ' {' + term + '} '
+			if i ==0:
+				self.ecoutput = re.sub(r"[^{]%s[^}]" %term, curly_term, target_body); #print self.ecoutput
+			else:
+				self.ecoutput = re.sub(r"[^{]%s[^}]" %term, curly_term, self.ecoutput); #print self.ecoutput
 
 
-		# Store this store this replace for future mappings.
-		# Apply previous search and replace mappings from a save file.
+	def ExtendBracketsInFile(self, file_name):
+		"""
+		"""
+		self.FindWordsInBracketsAndCurlies(self.raw)
+		self.ExtendBrackets(self.diagnoses, self.raw)
+		self.ExtendCurlys(self.symptoms, self.eboutput)
+		text_file = open(file_name, 'w')
+		text_file.write(self.ecoutput)
+		text_file.close()
 
 
--- test_importnlp.py
+++ test_importnlp.py
@@ -12,6 +12,7 @@ import basefunctions as bf
 class TestDatabase(unittest.TestCase):
 
 	def setUp(self):
+		self.file_name = '/Users/law826/Desktop/MSK.txt'
 		self.INLP = importnlp.ImportNLP('/Users/law826/Desktop/MSK.txt')
 
 	def test_SearchReplace(self):
@@ -20,18 +21,26 @@ class TestDatabase(unittest.TestCase):
 		self.assertEqual(self.INLP.replaced.count('fibromas'), 0)
 		self.assertEqual(self.INLP.replaced.count('fibroma'), 36)
 
-	def test_FindWordsInBrackets(self):
-		self.INLP.FindWordsInBrackets(self.INLP.raw)
+	def test_FindWordsInBracketsAndCurlies(self):
+		self.INLP.FindWordsInBracketsAndCurlies(self.INLP.raw)
+		#print self.INLP.diagnoses
+		#print self.INLP.symptoms
 
 	def test_ExtendBrackets(self):
 		list_of_terms = ['fibroma', 'apple']
-		target_body = 'fibroma apple orange'
+		target_body = '[fibroma] apple orange'
 		self.INLP.ExtendBrackets(list_of_terms, target_body)
 		self.assertEqual(self.INLP.eboutput, ' [fibroma] [apple] orange ')
 
+	def test_ExtendCurlys(self):
+		list_of_terms = ['fibroma', 'apple']
+		target_body = 'fibroma {apple} orange'
+		self.INLP.ExtendCurlys(list_of_terms, target_body)
+		self.assertEqual(self.INLP.ecoutput, ' {fibroma} {apple} orange ')
 
-		#self.assertEqual(self.INLP.diagnoses, None)
-		#self.assertEqual(self.INLP.symptoms, None)
+	def test_ExtendBracketsInFile(self):
+		self.INLP.ExtendBracketsInFile(self.file_name)
+		os.system("open " + self.file_name)
 
 	def tearDown(self):
 		"""
